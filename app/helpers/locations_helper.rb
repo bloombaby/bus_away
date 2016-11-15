63e61f14eb89cb5d3b5d483203fedbaf1a8c887b
Bre <bloombaby6789@gmail.com>
2016-11-14 20:02:04 -0600

    Set up locations helper

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,18 @@
 module LocationsHelper
+  # Parse the API data and store it so we can work with it
+  def fetch_buses_from_api(source_url)
+    http = Nett::HTTP.get_response(URI.parse(source_url))
+    data = http.body
+    JSON.parse(data)
+  end
+
+  def is_nearby?(user_lat, user_long, bus_lat, bus_long)
+    # Maximum Distance in degrees
+    max_distance = 0.01 # 0.01 deg is about 0.5 miles
+
+    difference_latitudes = user_lat - bus_lat.to_f
+    difference_longitudes = user_long - bus_long.to_f
+    distance = Math.sqrt(difference_latitudes ** 2 + difference_longitudes ** 2)
+    distance <= max_distance
+  end
 end
